45dacda1bd70d144485213e8a1f1a7c43e809094
abid7 <abidshahriar7@gmail.com>
2020-11-09 00:25:12 +0600

    added a comment

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// connecting to database
+// connecting to database and listening to port
 mongoose
   .connect(URI, {
     useNewUrlParser: true,
